529b4656c352c9aba6300dcaf8f28c1d7e46e6fa
Jarvis2207 <guilherme.hlg.eng@gmail.com>
2022-12-06 06:58:51 -0300

    starts Login chapter

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -13,6 +14,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     HomeRoutingModule
   ],
   exports: [HomeComponent]
